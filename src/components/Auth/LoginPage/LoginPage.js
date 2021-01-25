6758d9aa6515e0ed7f8b3f3b28b485039970939a
woasidh <woasidh@ajou.ac.kr>
2021-01-26 04:16:20 +0900

    loginPage updated

--- src/components/Auth/LoginPage/LoginPage.js
+++ src/components/Auth/LoginPage/LoginPage.js
@@ -7,25 +7,24 @@ function LoginPage() {
             <div className="loginBox">
                 <div className="login__title">
                     로그인
-            </div>
+                </div>
                 <input
                     type="text"
                     placeholder="아이디"
                     id="id"
-                    name="id" />
+                    name="id" 
+                    autocomplete="off"
+                    />
                 <input
-                    type="text"
+                    type="password"
                     placeholder="비밀번호"
                     id="pwd"
                     name="pwd" />
                 <div className="login__subMenu">
-                    <div className="login__saveID">
-
-                    </div>
                     <div className="login__help">
-                        <span>아이디 찾기</span>
-                        <span>비밀번호 찾기</span>
-                        <span>회원가입</span>
+                        <a href = "/auth/findId"><span>아이디 찾기</span></a>
+                        <a href = "/auth/findPwd"><span>비밀번호 찾기</span></a>
+                        <a href = "/auth/signup"><span>회원가입</span></a>
                     </div>
                 </div>
                 <button className="loginBtn">로그인</button>
