3ee5d393475baa0265d35ea5bb3209bda708f97c
woasidh <woasidh1@naver.com>
2021-02-13 18:15:27 +0900

    teacher updated on master branch

--- src/components/InfoPage/sections/CommunityPage/UnivPage/UnivPage.js
+++ src/components/InfoPage/sections/CommunityPage/UnivPage/UnivPage.js
@@ -1,114 +1,132 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function UnivPage() {
+
+    const [currentPage, setcurrentPage] = useState(1);
+    const [contentCount, setcontentCount] = useState(0);
+    const [contents, setcontents] = useState([]);
+    const [Pcontents, setPcontents] = useState([]);
+    const [searchCount, setsearchCount] = useState(10);
+    const [nonPriorityCount, setnonPriorityCount] = useState(0)
+
+    useEffect(() => {
+
+
+        axios.get('http://localhost:3001/univInfo/contents').then(response => {
+            if (response.data.success === true) {
+                console.log('axios comm success');
+                const total = response.data.result.length + response.data.resultP.length;
+                setcontentCount(total);
+                setsearchCount(10-response.data.resultP.length);
+                setnonPriorityCount(response.data.result.length)
+                setcontents(response.data.result.reverse());
+                setPcontents(response.data.resultP.reverse());
+            } else {
+                console.log('axios comm fail!');
+            }
+        })
+
+    }, [])
+
+    function genTableContent() {
+        let resultCode = [];
+        Pcontents.forEach((content, index) => {
+            content && resultCode.push(
+                <tr>
+                    <td>공지</td>
+                    <td>{content.title}</td>
+                    <td>{content.author}</td>
+                    <td>{content.genDate.split('T')[0]}</td>
+                    <td>{content.counter}</td>
+                </tr>
+            );
+        })
+        let j =0;
+        for (let i = searchCount * (currentPage - 1); i < searchCount * (currentPage); i++) {
+            contents[i] && resultCode.push(
+                <tr>
+                    <td>{nonPriorityCount-((currentPage-1)*searchCount)-j}</td>
+                    <td>{contents[i].title}</td>
+                    <td>{contents[i].author}</td>
+                    <td>{contents[i].genDate.split('T')[0]}</td>
+                    <td>{contents[i].counter}</td>
+                </tr>
+            );
+            j++;
+        }
+        return (
+            resultCode
+        );
+    }
+
+    function genPageSelector() {
+        const total = parseInt((nonPriorityCount-1)/searchCount);
+        let resultCode = [];
+        for(let i =1;i<total+2;i++){
+            if(i===currentPage){
+                resultCode.push(<button onClick = {()=>{
+                    setcurrentPage(i);
+                }}><li key = {i} className="pageSelector__number active">{i}</li></button>);
+            }else{
+                resultCode.push(<button onClick = {()=>{
+                    setcurrentPage(i);
+                }}><li key = {i} className="pageSelector__number">{i}</li></button>);
+            }
+        }
+        return (
+            resultCode
+        );
+    }
+
     return (
-        <div className="table">
-            <div className="table__intro">
-                <div className="table__counter">
-                    <p className="table__counter">
-                        Total
-                        <span> 120건</span>
+        <>
+            <div className="table">
+                <div className="table__intro">
+                    <div className="table__counter">
+                        <p className="table__counter">
+                            Total
+                        <span> {nonPriorityCount}건</span>
                         ,
-                        <span> 1</span>
-                        /120
-                    </p>
+                        <span> {currentPage} </span>
+                        page
+                        </p>
+                    </div>
+                    <div className="table__intro__search">
+                        <input type="text" placeholder="검색어를 입력해 주세요" id="search" name="name" />
+                        <button><i className="fas fa-search"></i></button>
+                    </div>
                 </div>
-                <div className="table__intro__search">
-                    <input type="text" placeholder="검색어를 입력해 주세요" id="search" name="name" />
-                    <button><i className="fas fa-search"></i></button>
-                </div>
+                <table>
+                    <colgroup>
+                        <col style={{ width: "10%" }} />
+                        <col style={{ width: "60%" }} />
+                        <col style={{ width: "10%" }} />
+                        <col style={{ width: "10%" }} />
+                        <col style={{ width: "10%" }} />
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th colSpan="1">공지</th>
+                            <th colSpan="1">제목</th>
+                            <th colSpan="1">글쓴이</th>
+                            <th colSpan="1">날짜</th>
+                            <th colSpan="1">조회</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {genTableContent()}
+                    </tbody>
+                </table>
             </div>
-            <table>
-                <colgroup>
-                    <col style={{ width: "10%" }} />
-                    <col style={{ width: "60%" }} />
-                    <col style={{ width: "10%" }} />
-                    <col style={{ width: "10%" }} />
-                    <col style={{ width: "10%" }} />
-                </colgroup>
-                <thead>
-                    <tr>
-                        <th colSpan="1">공지</th>
-                        <th colSpan="1">제목</th>
-                        <th colSpan="1">글쓴이</th>
-                        <th colSpan="1">날짜</th>
-                        <th colSpan="1">조회</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                    <tr>
-                        <td>147</td>
-                        <td>교습비 등 게시표</td>
-                        <td>관리자</td>
-                        <td>2020-10-12</td>
-                        <td>5312</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+            <ul className="pageSelector">
+                <button><div className="pageDirector"><i className="fas fa-angle-double-left"></i></div></button>
+                {currentPage !== 1 ? <button><div className="pageDirector"><i className="fas fa-angle-left"></i></div></button> : <></>}
+                {genPageSelector()}
+                {currentPage === parseInt((nonPriorityCount-1)/searchCount)-1 ? <button><div className="pageDirector"><i className="fas fa-angle-right"></i></div></button> : <></>}
+                <button><div className="pageDirector"><i className="fas fa-angle-double-right"></i></div></button>
+            </ul>
+        </>
     )
 }
 
--- src/components/InfoPage/sections/TeacherPage/TeacherPage.js
+++ src/components/InfoPage/sections/TeacherPage/TeacherPage.js
@@ -1,23 +1,212 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './TeacherPage.css'
 import avatar from '../../../../images/avatar.jpg'
+import axios from 'axios'
+import ads from '../../../../images/kakao.jpg'
 
 function TeacherPage() {
+
+    const [type, settype] = useState('science');
+    const [korean, setkorean] = useState('과학탐구')
+    const [subtype, setsubtype] = useState('physics');
+    const [science, setscience] = useState([]);
+    const [math, setmath] = useState([]);
+    const [english, setenglish] = useState([]);
+    const [language, setlanguage] = useState([]);
+    const [society, setsociety] = useState([]);
+    const [essay, setessay] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/teacher/contents').then(response => {
+            if (response.data.success === true) {
+                console.log(response.data)
+                setscience(response.data.result_science);
+                setmath(response.data.result_math);
+                setenglish(response.data.result_english);
+                setlanguage(response.data.result_english);
+                setsociety(response.data.result_society);
+                setessay(response.data.result_essay);
+            } else {
+                console.log('axios comm failed')
+            }
+        })
+    }, [])
+
+    function setType(e) {
+        switch (e.target.id) {
+            case '1':
+                settype('science');
+                setkorean('과학탐구')
+                break;
+            case '2':
+                settype('math');
+                setkorean('수학')
+                break;
+            case '3':
+                settype('english');
+                setkorean('영어')
+                break;
+            case '4':
+                settype('language');
+                setkorean('언어')
+                break;
+            case '5':
+                settype('society');
+                setkorean('사회')
+                break;
+            case '6':
+                settype('essay');
+                setkorean('논술')
+                break;
+            default:
+                break;
+        }
+    }
+
+    function setSubType(e) {
+        switch (e.target.id) {
+            case "1":
+                setsubtype('physics');
+                break;
+            case '2':
+                setsubtype('life');
+                break;
+            case '3':
+                setsubtype('chemistry');
+                break;
+            case '4':
+                setsubtype('earth');
+                break;
+            default:
+                break;
+        }
+    }
+
+    function genCards() {
+        console.log(type);
+
+        let teachers = [];
+        switch (type) {
+            case 'science':
+                let teachers_tmp = [];
+                switch (subtype) {
+                    case 'physics':
+                        science.forEach((teacher, index) => {
+                            if (teacher.subcategory === "physics") {
+                                teachers_tmp.push(teacher);
+                            }
+                        })
+                        break;
+                    case 'life':
+                        science.forEach((teacher, index) => {
+                            if (teacher.subcategory === "life") {
+                                teachers_tmp.push(teacher);
+                            }
+                        })
+                        break;
+                    case 'chemistry':
+                        science.forEach((teacher, index) => {
+                            if (teacher.subcategory === "chemistry") {
+                                teachers_tmp.push(teacher);
+                            }
+                        })
+                        break;
+                    case 'earth':
+                        science.forEach((teacher, index) => {
+                            if (teacher.subcategory === "earth") {
+                                teachers_tmp.push(teacher);
+                            }
+                        })
+                        break;
+                    default:
+                        break;
+                }
+                teachers = teachers_tmp;
+                break;
+            case 'math':
+                teachers = math;
+                break;
+            case 'english':
+                teachers = english;
+                break;
+            case 'language':
+                teachers = language;
+                break;
+            case 'society':
+                teachers = society;
+                break;
+            case 'essay':
+                teachers = essay;
+                break;
+            default:
+                break;
+        }
+
+        const result = teachers.map((teacher, index) => {
+            const careerArr = teacher.career.split('@@@');
+            const careerCode = careerArr.map((career) => {
+                return (
+                    <li>- {career}</li>
+                );
+            })
+            console.log(careerArr);
+            return (
+                <li className="teach__card">
+                    <div className="teach__card__subject">
+                        {korean}영역
+                    </div>
+                    <div className="teach__card__name">
+                        {teacher.teacherName} {teacher.teacherName === '김태호' ? '원장님' : '선생님'}
+                    </div>
+                    <ul className="teach__card__career">
+                        {careerCode}
+                    </ul>
+                    <div
+                        style={{ backgroundImage: "url('http://www.xn--4k0bv97bp8glte35a.kr/bbs/img/minimap_2.gif')" }}
+                        className="teach__card__avatar">
+
+                    </div>
+                    <div className="teach__card__msg">
+                        프로필 보기
+                    </div>
+                    <div className="teach__card__effect"></div>
+                </li>
+            );
+        })
+
+        return (
+            result
+        );
+    }
+
     return (
         <div className="teach">
             <div className="teach__title">
                 선생님 소개
             </div>
+            {/* <Button onClick = {
+                ()=>{
+                    console.log(type);
+                }
+            }>click me!</Button> */}
             <ul className="teach__subjects">
-                <button><li className="teach__subject__item">국어</li></button>
-                <button><li className="teach__subject__item">수학</li></button>
-                <button><li className="teach__subject__item">영어</li></button>
-                <button><li className="teach__subject__item">과학</li></button>
-                <button><li className="teach__subject__item">사회</li></button>
-                <button><li className="teach__subject__item">논술</li></button>
+                <button onClick={setType}><li key="1" id="1" className={"teach__subject__item " + (type === 'science' ? 'active' : '')}>과학</li></button>
+                <button onClick={setType}><li key="2" id="2" className={"teach__subject__item " + (type === 'math' ? 'active' : '')}>수학</li></button>
+                <button onClick={setType}><li key="3" id="3" className={"teach__subject__item " + (type === 'english' ? 'active' : '')}>영어</li></button>
+                <button onClick={setType}><li key="4" id="4" className={"teach__subject__item " + (type === 'language' ? 'active' : '')}>국어</li></button>
+                <button onClick={setType}><li key="5" id="5" className={"teach__subject__item " + (type === 'society' ? 'active' : '')}>사회</li></button>
+                <button onClick={setType}><li key="6" id="6" className={"teach__subject__item " + (type === 'essay' ? 'active' : '')}>논술</li></button>
             </ul>
+            {type === 'science' ? <ul className="teach__subsubjects">
+                <button onClick={setSubType}><li key="1" className={"teach__subsubject__item " + (subtype === 'physics' ? 'active' : '')} id="1">물리</li></button>
+                <button onClick={setSubType}><li key="2" className={"teach__subsubject__item " + (subtype === 'life' ? 'active' : '')} id="2">생명과학</li></button>
+                <button onClick={setSubType}><li key="3" className={"teach__subsubject__item " + (subtype === 'chemistry' ? 'active' : '')} id="3">화학</li></button>
+                <button onClick={setSubType}><li key="4" className={"teach__subsubject__item " + (subtype === 'earth' ? 'active' : '')} id="4">지구과학</li></button>
+            </ul> : <></>}
             <ul className="teach__container">
-                <li className="teach__card">
+                {genCards()}
+                {/* <li className="teach__card">
                     <div className="teach__card__subject">
                         과학탐구영역
                     </div>
@@ -37,195 +226,7 @@ function TeacherPage() {
                         프로필 보기
                     </div>
                     <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
-                <li className="teach__card">
-                    <div className="teach__card__subject">
-                        과학탐구영역
-                    </div>
-                    <div className="teach__card__name">
-                        김태호 선생님
-                    </div>
-                    <ul className="teach__card__career">
-                        <li>- 서울대 물리학과 졸업</li>
-                        <li>- 현) 김태호학원 원장</li>
-                    </ul>
-                    <div
-                        style={{ backgroundImage: `url(${avatar})` }}
-                        className="teach__card__avatar">
-
-                    </div>
-                    <div className="teach__card__msg">
-                        프로필 보기
-                    </div>
-                    <div className="teach__card__effect"></div>
-                </li>
+                </li> */}
             </ul>
         </div>
     )
